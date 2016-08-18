4fcd4bf8a3fc3a87019dc839c827769ec63c2fc4
Matthew Nerger <matthewjnerger3@gmail.com>
2016-08-18 18:10:18 -0400

    Begin to add local storage logic in story.service

--- app/story.service.ts
+++ app/story.service.ts
@@ -6,12 +6,33 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class StoryService {
+  hasStoredLocalStories: boolean = (localStorage["story.organizer.story.details"]);
+  hasStoredLocalIDs: boolean = (localStorage["story.organizer.story.IDs"]);
+
   getStories() {
-    return Promise.resolve(STORIES);
+    if(!this.hasStoredLocalStories) {
+      console.log("Using default STORIES.");
+      return Promise.resolve(STORIES);
+
+    } else {
+      var localStories = JSON.parse(localStorage["story.organizer.story.details"]);
+      console.log("Using local storage stories object:");
+      console.log(localStories);
+      return localStories;
+    }
   }
 
   getIDs() {
-    return Promise.resolve(LIST_IDS);
+    if(!this.hasStoredLocalIDs) {
+      console.log("Using default IDS.");
+      return Promise.resolve(LIST_IDS);
+
+    } else {
+      var localIDs = JSON.parse(localStorage["story.organizer.story.IDs"]);
+      console.log("Using local storage IDs object:");
+      console.log(localIDs);
+      return localIDs;
+    }
   }
 
   getStory(id: number) {
