2a038af741322ab422954ddf27705ca61f7f1801
Matthew Nerger <matthewjnerger3@gmail.com>
2016-08-21 21:40:56 -0400

    Finish remove series function

--- app/edit-series.component.ts
+++ app/edit-series.component.ts
@@ -47,31 +47,17 @@ export class EditSeriesComponent implements OnInit {
     var seriesLength = this.ids.series.length - 1;
     var ids = this.ids;
 
-    //TODO: Console.log the actual logic being used by the function (eg series = x; id = y; new array = /n Array)
-    //TODO: This will, hopefully, make it clearer where the error is
-
-    console.log(" ");
-    console.log(" ");
-    console.log("Function will:");
-
-    console.log(" - Remove series " + ids.series[id] + " from ids.series.");
-    this.ids.series.splice(id, 1);
-
     stories.forEach(function(story) {
       story.series.forEach(function(series, index) {
         if (series === id) {
-          console.log(" - Remove series " + ids.series[id] + " from " + story.title + ", which is in positon " + index + " of series array.");
           story.series.splice(index, 1);
         } else if (series > id) {
-          console.log(" - Reduce the series id of " + ids.series[series] + " in the array for " + story.title + ".");
-          console.log("Original id: " + ids.series[series]);
-          ids.series[series]--
-          console.log("New id: " + ids.series[series]);
+          story.series[index]--;
         }
       });
     });
 
-    console.log(" - Save new stories and ids array to local storage.")
+    this.ids.series.splice(id, 1);
     this.storyService.saveStories(stories, this.ids);
   }
 }
